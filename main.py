931e5bd5f30f731d32d55f041ce56c8627ce4fd5
ndvo <nelson@ocastudios.com>
2019-06-02 22:01:23 +0000

    Post and Get requests

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import flask
 from flask import Flask
 from flask import request
-app = Flask(__name__, template_folder="templates")
+from sqlalchemy import asc, desc
+app = Flask(__name__, template_folder="templates", static_url_path="")
 
 
 import models
 from models import session
 
-class Page(): pass
+class Page():
+    title = ""
+    content = ""
+    model = ""
 
+    
 
 @app.route("/")
 def hello():
     categories = session.query(models.Category).all()
-    return "Home page";
+    page = Page()
+    page.title = "Welcome"
+    page.description = "A categorization application, that stores items into categories."
+    page.content = Page()
+    page.content.title = "Welcome to the Category App"
+    page.content.main = "<p>This application allows you to organize items into categories and retrieve them properly.</p><h2>Recent items</h2>"
+    categories = session.query(models.Category).order_by(asc(models.Category.name)).all()
+    for c in categories:
+        c.to_link()
+    page.content.aside = "<h2>Categories</h2>"
+    page.content.aside += flask.render_template('list.html', List=[flask.render_template('link.html', link=c) for c in categories])
+    items = list(session.query(models.Item).order_by(desc(models.Item.id)).limit(10))
+    for i in items:
+        i.to_link()
+    page.content.main += flask.render_template('list.html', List=[flask.render_template('link.html', link=i) for i in items])
+    return flask.render_template('base.html', page=page);
+
+@app.route("/style.css")
+def css():
+    return flask.send_from_directory("", 'style.css')
 
 @app.route("/categories", methods=['GET', 'POST'])
 def categories():
@@ -24,7 +48,8 @@ def categories():
         page.title = "Categories page";
         page.description = "The full list of the categories available"
         categories = session.query(models.Category).all()
-        categories_names = [c.name for c in categories]
+        for i in categories: i.to_link()
+        categories_names = [flask.render_template('link.html', link=c)  for c in categories]
         page.content = Page()
         page.content.title = "Categories"
         page.content.main = flask.render_template('list.html', List=categories_names)
@@ -32,30 +57,41 @@ def categories():
         return flask.render_template('base.html', page=page)
 
 @app.route("/categories/add", methods=['GET', 'POST'])
-def caterogy_form():
+def category_form():
     if request.method == 'POST':
         new_category = models.Category(name = request.form['name'], description=request.form['description'])
         session.add(new_category)
         session.commit()
-        return """
-        Success. Your data has been received.
-        """
+        page = Page()
+        page.content = Page()
+        page.title = page.content.title = 'Success'
+        page.content.main = flask.render_template('success.html')
+        return flask.render_template('base.html', page=page)
     else:
-        return """
-        <form method="POST" action="/categories/add" enctype="multipart/formdata">
-        <label for="name">Name</label>
-        <input type="text" name="name" placeholder="Choose a name for your category" />
-        <textarea name="description"></textarea>
-        <button type="submit">Submit</button>
-        </form>
-        """;
+        page = Page()
+        page.content = Page()
+        page.title = "Create a new category"
+        page.description = "This page allows the creation of new categories to the application"
+        page.content.title = "New category"
+        page.content.main = flask.render_template('form_category.html')
+        return flask.render_template('base.html', page=page)
 
 @app.route("/category/<category>", methods=['GET', 'PUT', 'DELETE'])
 def category(category=None):
     if request.method == 'POST':
         pass
     if request.method == 'GET':
-        return "Category page";
+        page = Page()
+        category = session.query(models.Category).get(category)
+        category.load_items_links()
+        page.title = category.name
+        page.description = "Describes the category "+category.name
+        page.content = Page()
+        page.content.title = category.name
+        page.content.main = flask.render_template('category_main.html', category=category)
+        page.content.model = 'category'
+        page.content.id = category.id
+        return flask.render_template('base.html', page=page)
     if request.method == 'PUT':
         pass
     if request.method == 'DELETE':
@@ -67,23 +103,48 @@ def terms(category=None):
     if request.method == 'GET':
         return "term page "+category;
 
+@app.route("/categories/<category>/term/add", methods=['GET', 'POST'])
+def term_form(category=None):
+    if request.method == 'POST':
+        new_item = models.Item(name = request.form['name'], description=request.form['description'], category_id=int(category))
+        session.add(new_item)
+        session.commit()
+        page = Page()
+        page.content = Page()
+        page.title = page.content.title = 'Success'
+        page.content.main = flask.render_template('success.html')
+        return flask.render_template('base.html', page=page)
+    else:
+        category = session.query(models.Category).get(category)
+        page = Page()
+        page.content = Page()
+        page.title = "Create a new category"
+        page.description = "This page allows the creation of new categories to the application"
+        page.content.title = "New term in the category "+category.name
+        page.content.main = flask.render_template('form_term.html', category=category)
+        return flask.render_template('base.html', page=page)
+
+
 @app.route("/category/<category>/term/<term>", methods=['GET', 'PUT', 'DELETE'])
-def term(category=None):
+def term(category=None, term=None):
     if request.method == 'POST':
         pass
     if request.method == 'GET':
-        return "Category page";
+        page = Page()
+        category = session.query(models.Category).get(category)
+        category.to_link()
+        category.link = flask.render_template('link.html', link=category)
+        term = session.query(models.Item).get(term)
+        page.title = category.name+": "+term.name
+        page.description = "This page describes "+term.name+" from the category "+category.name
+        page.content = Page()
+        page.content.title = term.name
+        page.content.main = flask.render_template('term.html', category=category, item=term)
+        return flask.render_template('base.html', page=page)
     if request.method == 'PUT':
         pass
     if request.method == 'DELETE':
         return "Category page";
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
 	app.debug = True
--- models.py
+++ models.py
@@ -1,11 +1,11 @@
-
+import flask
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
-engine = create_engine('sqlite:///base.db')
+engine = create_engine('sqlite:///base.db?check_same_thread=False')
 
 DBsession = sessionmaker()
 DBsession.bind = engine
@@ -18,6 +18,24 @@ class Category(Base):
     name = Column(String(255))
     description = Column(Text())
 
+    def load_items(self):
+        try:
+            return self.items
+        except AttributeError:
+            self.items = session.query(Item).filter_by(category_id = self.id)
+            return self.items
+
+    def load_items_links(self):
+        self.items_links = []
+        for i in self.load_items():
+            i.to_link()
+            self.items_links.append(flask.render_template('link.html', link=i))
+
+    def to_link(self):
+        self.href = '/category/'+str(self.id)
+        self.text = self.name
+        self.title = 'Visit '+self.name+' page'
+        self.htmlclass = 'category'
 
 
 class Item(Base):
@@ -27,5 +45,12 @@ class Item(Base):
     description = Column(Text())
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship(Category)
+
+    def to_link(self):
+        self.href = '/category/'+str(self.category_id)+'/term/'+str(self.id)
+        self.text = self.name
+        self.title = 'Visit '+self.name+' page'
+        self.htmlclass = 'item'
+
 
 Base.metadata.create_all(engine)
